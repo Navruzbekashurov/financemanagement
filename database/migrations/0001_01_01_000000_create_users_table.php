48ca2c1a275f099349c0cd698ed5a6959b628e50
Ashurov Navruzbek <ashurov.n@sablab.org>
2025-10-16 12:43:59 +0500

    add migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('timezone')->default('Asia/Tashkent');
+            $table->string('preferred_language',10)->default('uz');
             $table->rememberToken();
             $table->timestamps();
         });
